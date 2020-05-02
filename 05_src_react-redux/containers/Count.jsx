82a6f5c71105ae107e0cf6bdb96c55ae7788ed3b
yun88Xiao88Chu <1520420636@qq.com>
2020-05-02 22:13:54 +0800

    react-redux容器组件dispatch方法的简写

--- 05_src_react-redux/containers/Count.jsx
+++ 05_src_react-redux/containers/Count.jsx
@@ -9,6 +9,25 @@ import { increment,decrement } from "../redux/actions/count";
 export default connect(
   state => ({count:state}),
   
+  //原始的写法：
+	/* dispatch => (
+		{
+			increment:(value)=>{dispatch(increment(value))},
+			decrement:(value)=>{dispatch(decrement(value))},
+		}
+	) */
   {increment,decrement} 
   
   )(Count)
+
+  /* 以上精简的原理类似: (全是react-redux底层做的) 
+	function connect({a,b}){
+ *	//引入了dispatch
+		return (
+			dispatch => ({
+				a: (value) => { dispatch( a(value) ) },
+				b: (value) =>{ dispatch( a(value) ) }
+			})
+		)
+	} 
+*/
